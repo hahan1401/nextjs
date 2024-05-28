6976c8131c0760bed73396e03cf50f46ff878304
hahan <hanvietha141@gmail.com>
2024-05-28 22:31:47 +0700

    (+) upgrade react+next to RC version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import CoreProvider from './CoreProvider';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +20,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <CoreProvider>
           <div className='p-10'>{children}</div>
-          {/* <ReactQueryDevtools initialIsOpen={true} /> */}
         </CoreProvider>
       </body>
     </html>
